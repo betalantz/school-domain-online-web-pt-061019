b3dc9bd0111966632e3c9b6ca108b6c22d2033f5
Lantz Warrick <betalantz@gmail.com>
2019-07-09 22:34:22 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,6 +7,10 @@ class School
   end
 
   def add_student(student_name, grade)
-    # @roster[:grade = student_name]
+    if @roster[:grade] == nil
+      @roster[:grade] = [student_name]
+    else
+      @roster[:grade] << student_name
+    end
   end
 end
